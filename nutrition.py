fb1d9336dfd14a87a6d6a2d377cdd89d9ca493d5
Sergey Gerodes <sergey.gerodes@tum.de>
2017-09-09 15:56:20 +0200

    data anlytic json file

---
+++ nutrition.py
@@ -0,0 +1,36 @@
+
+
+nutrition = {
+    #calories are in 100 gramms
+    #all other in percentage
+    "apple" : {
+        "calories" : 65 ,
+        "proteins" : 2 ,
+        "carbs" : 95 ,
+        "fats" : 3
+    },
+    "banana" : {
+        "calories" : 200,
+        "proteins" : 4,
+        "carbs" : 93,
+        "fats" : 3
+    },
+    "orange" : {
+        "calories" : 85,
+        "proteins" : 7,
+        "carbs" : 91,
+        "fats" : 2
+    },
+    "4" : {
+        "calories" : 0,
+        "proteins" : 0,
+        "carbs" : 0,
+        "fats" : 0
+    },
+    "5" : {
+        "calories" : 0,
+        "proteins" : 0,
+        "carbs" : 0,
+        "fats" : 0
+    }
+}
